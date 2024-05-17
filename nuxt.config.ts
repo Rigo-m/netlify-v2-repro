9c836093129866031181ad955c8c78d3e6563984
Rigo-m <matteo.rigoni@atoms.retex.com>
2024-05-17 16:21:13 +0200

    added catch all route to routerules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,5 +12,12 @@ export default defineNuxtConfig({
         "Cdn-Cache-Control": "public, s-maxage=31536000, must-revalidate",
       },
     },
+    "/**": {
+      isr: 3600,
+      headers: {
+        "Netlify-Vary": "query=page",
+        "Cdn-Cache-Control": "public, s-maxage=31536000, must-revalidate",
+      },
+    },
   },
 });
